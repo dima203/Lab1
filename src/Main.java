76ae5fa164d248fcbd77e22aa58cc9c1e6888d29
dima203 <gg37500@yandex.by>
2023-10-24 23:13:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array1 = {3, 3, 3, 6, 8, 1};
+
+        Main main = new Main();
+        System.out.println(main.getPercentGreaterMiddle(array1));
+        double[][] matrix = main.single(7);
+
+        String test = "primer stroki";
+        System.out.println(main.capitalize(test));
+    }
+
+    public float getPercentGreaterMiddle(int[] numbers) {
+        int average = (int)Arrays.stream(numbers).average().getAsDouble();
+        int count = 0;
+
+        for (int number : numbers) {
+            if (number > average) {
+                count++;
+            }
+        }
+
+        return ((float)count / numbers.length) * 100;
+    }
+
+    public double[][] single(int size) {
+        double [][] matrix = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+        return matrix;
+    }
+
+    public String capitalize(String str) {
+        String[] words = str.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            String firstChar = words[i].substring(0, 1);
+            firstChar = firstChar.toUpperCase();
+            words[i] = firstChar + words[i].substring(1);
+        }
+        return String.join(" ", words); // Hello Java
+    }
+}
